1a68e59b07ae65982f3256b04a7fd6b0067e9327
artm1904 <artmsavchenko@yandex.ru>
2026-01-05 20:21:06 +0300

    feat: Add custom logging class to gateway_service

---
+++ gateway_service/src/internal/logger/PrettyLogger.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+
+#include "ansi_colors.h"
+#include "oatpp/Environment.hpp"
+
+class PrettyLogger : public oatpp::Logger {
+   public:
+    void log(v_uint32 priority, const std::string& tag, const std::string& message) override {
+        auto now = std::chrono::system_clock::now();
+        auto now_c = std::chrono::system_clock::to_time_t(now);
+        auto now_tm = *std::localtime(&now_c);
+
+        switch (priority) {
+            case PRIORITY_V:
+            case PRIORITY_D:
+                std::cout << RESET << GRAY << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << " ["
+                          << tag << "] " << message << std::endl;
+                break;
+
+            case PRIORITY_I:
+                std::cout << RESET << GREEN << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << " ["
+                          << tag << "] " << message << std::endl;
+                break;
+
+            case PRIORITY_W:
+                std::cout << RESET << YELLOW << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << " ["
+                          << tag << "] " << message << std::endl;
+                break;
+
+            case PRIORITY_E:
+                std::cout << RESET << RED << BOLD << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S")
+                          << " [" << tag << "] " << message << std::endl;
+                break;
+
+            default:
+                std::cout << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << " [" << tag << "] "
+                          << message << std::endl;
+        }
+    }
+};
---
+++ gateway_service/src/internal/logger/ansi_colors.h
@@ -0,0 +1,21 @@
+#pragma once 
+
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define GRAY    "\033[90m"
+
+
+#define BOLD    "\033[1m"
+#define ITALIC  "\033[3m"
+#define UNDERLINE "\033[4m"
+#define BLINK   "\033[5m"
+#define INVERT  "\033[7m"
+#define HIDDEN  "\033[8m"
+#define STRIKETHROUGH "\033[9m"
